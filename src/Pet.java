f55aec49be92e3c9c41fdc8ad6e5b653471348d8
Sdriver1 <sjdriver08@gmail.com>
2025-03-04 11:28:10 -0500

    Adding kennel work so far

---
+++ src/Pet.java
@@ -0,0 +1,56 @@
+public abstract class Pet {
+    
+    private String myName;
+    
+    public Pet(String name) {
+        myName = name;
+    }
+    
+    public String getName() {
+        return myName;
+    }
+    
+    public abstract String speak();
+    
+}
+
+class Cat extends Pet {
+    public Cat(String name) {
+        super(name);
+    }
+    
+    public String speak() {
+        return "meow";
+    }
+}
+
+class Dog extends Pet {
+    public Dog(String name) {
+        super(name);
+    }
+    
+    public String speak() {
+        return "bark";
+    }
+}
+
+class LoudDog extends Dog {
+    public LoudDog(String name) {
+        super(name);
+    }
+    
+    public String speak() {
+        return super.speak() + " " + super.speak();
+    }
+}
+
+class BadDog extends LoudDog {
+    public BadDog(String name) {
+        super(name);
+    }
+    
+    public String jump() {
+        return "jumps";
+    }
+}
+
